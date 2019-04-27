00dfb847385f8ba0a42f4dc9bfc2868a04320aef
katabaticwind <fastman@fastmail.com>
2019-04-27 07:55:44 -0400

    Added environments files

---
+++ layers.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+def dense(inputs, hidden_units, activation, dtype = tf.float32):
+    """Create a fully-connected layer.
+
+        # Arguments
+        - `inputs::Tensor`: assumed to have shape (None, inputs_units).
+        - `hidden_units::Int`: number of hidden units in layer.
+        - `activation`: activation to apply following inner product.
+
+        # Returns
+        - `output (tf.Tensor)`
+    """
+    inputs_units = tf.shape(inputs)[1]
+    weights = tf.Variable(name='weights', shape=(input_units, hidden_units), dtype = dtype)
+    bias = tf.Variables(name='bias', shape=(hidden_units, ), dtype=dtype)
+    return activation(tf.add(bias, tf.matmul(inputs, weights)))
+
+def conv2d(inputs):
+    pass
