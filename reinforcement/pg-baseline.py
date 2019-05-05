d1092f2232174fab82bc866b3d7393840881c7e4
katabaticwind <fastman@fastmail.com>
2019-05-04 20:18:38 -0400

    Adding CNN for Atari.

--- reinforcement/pg-baseline.py
+++ reinforcement/pg-baseline.py
@@ -11,7 +11,9 @@ tf.logging.set_verbosity(tf.logging.ERROR)  # suppress annoying messages
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('mode', 'train', """'Train' or 'test'.""")
+tf.app.flags.DEFINE_boolean('atari', False, """Flag for Atari environments.""")
 tf.app.flags.DEFINE_string('env_name', 'CartPole-v0', """Gym environment.""")
+tf.app.flags.DEFINE_string('filters', '32,64', """Number of filters in CNN layers.""")
 tf.app.flags.DEFINE_string('hidden_units', '32', """Size of hidden layers.""")
 tf.app.flags.DEFINE_float('learning_rate', '1e-2', """Initial learning rate.""")
 tf.app.flags.DEFINE_integer('batches', 100, """Batches per training update.""")
@@ -35,11 +37,52 @@ def state_dimensions(env):
     """Find the number of dimensions in the state."""
     return env.observation_space.shape[0]
 
+def process_atari(state):
+    return np.mean(state[1:-1, :, :], axis=2) / 255
+
 def mlp(x, sizes, activation=tf.tanh, output_activation=None):
     """Build a feedforward neural network."""
     for size in sizes[:-1]:
         x = tf.layers.dense(x, units=size, activation=activation)  # TODO: creates multiple warnings (DEPRECATED)
     return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
+
+def cnn(x, filters, units, activation=tf.nn.relu, output_activation=None):
+
+    # x shape is: batch_size, 208 - 2, 160, 1 or 3?
+    x = tf.reshape(x, (-1, 208, 160, 1))
+
+    # conv1
+    x = tf.layers.conv2d(
+        inputs=x,
+        filters=filters[0],
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+    x = tf.layers.max_pooling2d(
+        inputs=x,
+        pool_size=[2, 2],
+        strides=2
+    )  # batch_size x 104 x 80 x filters[0]
+
+    # conv2
+    x = tf.layers.conv2d(
+        inputs=x,
+        filters=filters[1],
+        kernel_size=[5, 5],
+        padding='same',
+        activation=tf.nn.relu)
+    x = tf.layers.max_pooling2d(
+        inputs=x,
+        pool_size=[2, 2],
+        strides=2
+    )  # batch_size x 52 x 40 x filters[1]
+
+    # dense
+    x = tf.reshape(x, (-1, 52 * 40 * filters[1]))  # flatten x
+    x = tf.layers.dense(x, units[0], tf.nn.relu)  # 1024
+
+    # logits
+    return tf.layers.dense(x, units[-1], activation=output_activation)
 
 def reward_to_go(rewards):
     """Calculate the cumulative reward at each step."""
@@ -161,6 +204,122 @@ def train(env_name='CartPole-v0', hidden_units=[32], learning_rate=1e-2, batches
             saver.save(sess, save_path=save_path + 'vpg-baseline-' + env_name)
             return saver.last_checkpoints
 
+def train_cnn(env_name='Breakout-v0', filters=[32, 64], hidden_units=[1024], learning_rate=1e-2, batches=100, batch_size=128, mini_batch_size=32, save_path=None, render=False):
+
+    """
+    The state space if huge for Atari games: 210 x 160 x 3. Even with down-sampling, we don't be able to use anything like 5000 samples in a batch. What we'll have to do is run a large number of steps, then perform updates in mini-batches of size (e.g.) Alternatively, we could collect the gradient updates for each mini-batch and perform one update for all 5000 observations. We need to play at least one episode though in order to get weights of update.
+    """
+
+    # create an environment
+    env = gym.make(env_name)
+    n_actions = available_actions(env)
+
+    # create placeholders
+    states_pl = tf.placeholder(tf.float32, (None, 208, 160))  # or 3?
+    actions_pl = tf.placeholder(tf.int32, (None, ))
+    weights_pl = tf.placeholder(tf.float32, (None, ))
+
+    # create a policy network
+    logits = cnn(states_pl, filters, hidden_units + [n_actions])
+    actions = tf.squeeze(tf.multinomial(logits=logits, num_samples=1), axis=1)
+
+    # create a value network
+    values = cnn(states_pl, filters, hidden_units + [1])
+
+    # define policy network training operation
+    actions_mask = tf.one_hot(actions_pl, n_actions)
+    log_probs = tf.reduce_sum(actions_mask * tf.nn.log_softmax(logits), axis=1)
+    policy_loss = -tf.reduce_mean((weights_pl - values) * log_probs)
+    policy_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(policy_loss)
+
+    # define value network training operation
+    value_loss = tf.losses.mean_squared_error(weights_pl, tf.squeeze(values, axis=1))
+    value_train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(value_loss)
+
+    # create a saver
+    saver = tf.train.Saver()
+
+    # define core functions
+    def run_episode(env, sess, render=False):
+        state = env.reset()
+        episode_states = [process_atari(state)]
+        episode_actions = []
+        episode_weights = []
+        total_reward = 0
+        total_steps = 0
+        if render == True:
+            env.render()
+        while True:
+            action = sess.run(actions, feed_dict={states_pl: process_atari(state).reshape(-1, 208, 160)})[0]
+            state, reward, done, info = env.step(action)  # step requires scalar action
+            if render == True:
+                env.render()
+            episode_states += [process_atari(state.copy())]  # copy needed?
+            episode_actions += [action]
+            episode_weights += [reward]
+            total_reward += reward
+            total_steps += 1
+            if done:
+                break
+        return episode_states, episode_actions, reward_to_go(episode_weights), total_reward, total_steps
+
+    def run_batch(env, sess, render=False):
+        t0 = time.time()
+        batch_states = []
+        batch_actions = []
+        batch_weights = []
+        batch_rewards = []
+        batch_steps = []
+        episodes = 0
+        while len(batch_weights) < batch_size:
+            if episodes == 0:
+                episode_states, episode_actions, episode_weights, total_reward, total_steps = run_episode(env, sess, render=render)
+            else:
+                episode_states, episode_actions, episode_weights, total_reward, total_steps = run_episode(env, sess, render=False)
+            episodes += 1
+            batch_states.extend(episode_states[:-1])  # only keep states preceeding each action
+            batch_actions.extend(episode_actions)
+            batch_weights.extend(episode_weights)
+            batch_rewards.append(total_reward)
+            batch_steps.append(total_steps)
+        t = time.time()
+        return batch_states, batch_actions, batch_weights, np.mean(batch_rewards), np.mean(batch_steps), episodes, t - t0
+
+    def update_policy_network(states, actions, weights, sess):
+        idx = 0
+        while idx < batch_size:
+            feed_dict = {
+                states_pl: states[idx:idx + mini_batch_size],
+                weights_pl: weights[idx:idx + mini_batch_size],
+                actions_pl: actions[idx:idx + mini_batch_size]
+            }
+            batch_loss, _ = sess.run([policy_loss, policy_train_op], feed_dict=feed_dict)
+            idx += mini_batch_size
+
+    def update_value_network(states, weights, sess):
+        idx = 0
+        while idx < batch_size:
+            feed_dict = {
+                states_pl: states[idx:idx + mini_batch_size],
+                weights_pl: weights[idx:idx + mini_batch_size],
+            }
+            batch_loss, _ = sess.run([value_loss, value_train_op], feed_dict=feed_dict)
+            idx += mini_batch_size
+
+    # train the network
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for batch in range(batches):
+            batch_states, batch_actions, batch_weights, batch_reward, batch_steps, batch_episodes, batch_time = run_batch(env, sess, render)
+            update_value_network(batch_states, batch_weights, sess)
+            update_policy_network(batch_states, batch_actions, batch_weights, sess)
+            print("batch = {:d},    reward = {:.2f} (mean),    steps = {:.2f} (mean),   time = {:.2f}".format(batch, batch_reward, batch_steps, batch_time))
+        if save_path is not None:
+            saver.save(sess, save_path=save_path + 'vpg-baseline-' + env_name)
+            return saver.last_checkpoints
+
+
+
 def test(env_name='CartPole-v0', hidden_units=[32], episodes=100, load_path=None, render=False):
     """
     Load and test a trained model from checkpoint files.
@@ -221,14 +380,24 @@ def test(env_name='CartPole-v0', hidden_units=[32], episodes=100, load_path=None
 
 
 if __name__ == '__main__':
+    filters = [int(i) for i in FLAGS.filters.split(',')]
     hidden_units = [int(i) for i in FLAGS.hidden_units.split(',')]
     if FLAGS.mode == 'train':
-        checkpoint_file = train(env_name=FLAGS.env_name,
-                                hidden_units=hidden_units,
-                                learning_rate=FLAGS.learning_rate,
-                                batches=FLAGS.batches,
-                                save_path=FLAGS.save_path,
-                                render=FLAGS.render)
+        if FLAGS.atari:
+            checkpoint_file = train_cnn(env_name=FLAGS.env_name,
+                                        filters=filters,
+                                        hidden_units=hidden_units,
+                                        learning_rate=FLAGS.learning_rate,
+                                        batches=FLAGS.batches,
+                                        save_path=FLAGS.save_path,
+                                        render=FLAGS.render)
+        else:
+            checkpoint_file = train(env_name=FLAGS.env_name,
+                                    hidden_units=hidden_units,
+                                    learning_rate=FLAGS.learning_rate,
+                                    batches=FLAGS.batches,
+                                    save_path=FLAGS.save_path,
+                                    render=FLAGS.render)
         print('Checkpoint saved to {}'.format(checkpoint_file))
     elif FLAGS.mode == 'test':
         rewards = test(env_name=FLAGS.env_name,
